8c87c64c5feff2ace234699e3c79ec4e43bf4fee
dunglevan-design <dunglevan2001@gmail.com>
2021-11-29 06:42:29 +0700

    .

--- src/TopCountriesBarGraph.js
+++ src/TopCountriesBarGraph.js
@@ -26,8 +26,8 @@ export const options = {
   plugins: {
     legend: {
       display: true,
-      title: {
-        fontColor: "#fff",
+      labels: {
+        color: "#fff",
       },
     },
   },
